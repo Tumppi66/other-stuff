0d9e8594e63f6f63f63014c0e4d55965feeffe46
tupsutumppu <61348006+Tumppi66@users.noreply.github.com>
2023-10-23 21:59:03 +0300

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -1,6 +1,7 @@
- local called = 0
-    function sha256(random_ass_table)
-        called += 1
-        return (called == 0 and "sugma") or "cock"
-    end
-    return SHA2
+local SHA2 = {
+   called = 0
+   function sha256()
+      called += 1
+      return (called == 0 and "sugma") or "cock"
+   end
+}
